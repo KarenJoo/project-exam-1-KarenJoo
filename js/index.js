289285333bd9a6c0a0cb1c104b4e4c2903039164
Karen Jo <karenfiksdahl@gmail.com>
2023-05-12 16:26:11 +0200

    slider created, js adjustments

--- js/index.js
+++ js/index.js
@@ -20,36 +20,38 @@ fetch("https://health-hub.karenjo.no/wp-json/wp/v2/posts?_embed&per_page=6")
     });
     // appending post items to latest container
     postItems.forEach((postItem) => {
-      latestPostsContainer.append(postItem);
+      slides.append(postItem);
     });
     // slider using step to display 1 of the latest posts
     // maxposition displays the sliders total 6 latest posts
     let currentPosition = 0;
-    const step = latestPostsContainer.offsetWidth / 1;
-    const maxPosition = postItems.length - 6;
+    const step = postItems[0].offsetWidth;
+    const maxPosition = postItems.length - 1;
 
     prevBtn.addEventListener("click", () => {
       if (currentPosition > 0) {
         currentPosition--;
-        latestPostsContainer.style.transform = `translateX(-${
-          currentPosition * step
-        }px)`;
+        slides.style.transform = `translateX(-${currentPosition * step}px)`;
         // moves the step to the new currentposition to view the next content
       }
-      document.querySelector("#prevBtn").addEventListener("click", () => {
-        plusSlides(-1);
-      });
     });
 
     nextBtn.addEventListener("click", () => {
       if (currentPosition < maxPosition) {
         currentPosition++;
-        latestPostsContainer.style.transform = `translateX(-${
-          currentPosition * step
-        }px)`;
+        slides.style.transform = `translateX(-${currentPosition * step}px)`;
       }
     });
   });
+
+// nextBtn.addEventListener("click", () => {
+//   if (currentPosition < maxPosition) {
+//     currentPosition++;
+//     latestPostsContainer.style.transform = `translateX(-${
+//       currentPosition * step
+//     }px)`;
+//   }
+// });
 
 // function to the slider with addeventlistner to prevBtn and nextBtn, and slideIndex to add functionality
 
@@ -61,16 +63,27 @@ function plusSlides(n) {
 }
 
 function showSlides(n) {
-  slideIndex = 1;
+  const slides = latestPostsContainer.querySelectorAll(".post-item");
+
+  if (n < slides.length) {
+    slideIndex = 1;
+  }
 
   if (n < 1) {
     slideIndex = slides.length;
   }
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
+
+  slides.forEach((slide) => {
+    slide.style.display = "none";
+  });
 
   slides[slideIndex - 1].style.display = "block";
 }
 
-document.querySelector("#prevBtn").addEventListener("click");
+prevBtn.addEventListener("click", () => {
+  plusSlides(-1);
+});
+
+nextBtn.addEventListener("click", () => {
+  plusSlides(1);
+});
