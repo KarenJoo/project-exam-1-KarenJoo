9ca96fb32c79fc461e4d4719f6892dfa0439c3ab
Karen Jo <karenfiksdahl@gmail.com>
2023-05-16 22:30:42 +0200

    blogcontent styled when clicket prevbtn

--- js/index.js
+++ js/index.js
@@ -13,7 +13,6 @@ fetch("https://health-hub.karenjo.no/wp-json/wp/v2/posts?_embed&per_page=6")
     // storing the posts data in a variable to control the length of the posts arrays and the next btn
     postsData = posts;
 
-    // loader
     sliderContainer.innerHTML = " ";
 
     posts.slice(0, 3).forEach((post) => {
@@ -29,7 +28,6 @@ fetch("https://health-hub.karenjo.no/wp-json/wp/v2/posts?_embed&per_page=6")
       postElement.append(postLink);
       sliderContainer.append(postElement);
     });
- 
 
     // adding event listner to next button, displaying 3 blogposts
     const nextBtn = document.getElementById("nextBtn");
@@ -72,6 +70,7 @@ fetch("https://health-hub.karenjo.no/wp-json/wp/v2/posts?_embed&per_page=6")
       // display the rev 3 blogposts
       posts.slice(counter, counter + 3).forEach((post) => {
         const postElement = document.createElement("div");
+        postElement.classList.add("slider-content");
         postElement.innerHTML = `
         <h2>${post.title.rendered}</h2>
         <img src="${post._embedded["wp:featuredmedia"][0].source_url}" alt="${post.title.rendered}">
@@ -85,50 +84,3 @@ fetch("https://health-hub.karenjo.no/wp-json/wp/v2/posts?_embed&per_page=6")
     // enable the next btn
     prevBtn.disabled = true;
   });
-
-//     posts.forEach((post, index) => {
-//       if (index < 3) {
-//         const postItem = document.createElement("div");
-//         postItem.classList.add("post-item");
-//         postItem.innerHTML = `<a href="${post.link}">
-//     <img src="${post._embedded["wp:featuredmedia"][0].source_url}" alt="${post.title.rendered}">
-//           <h3>${post.title.rendered}</h3>
-//         </a>`;
-//         postItems.push(postItem);
-//       }
-//     });
-//     // appending post items to latest container
-//     postItems.forEach((postItem) => {
-//       slides.append(postItem);
-//     });
-//     // slider using step to display 1 of the latest posts
-//     // maxposition displays the sliders total 3 latest posts
-//     let currentPosition = 0;
-//     const step = postItems[0].offsetWidth;
-
-//     updateMaxPosition();
-
-//     nextBtn[i].addEventListener("click", () => {
-//       if (currentPosition < maxPosition) {
-//         currentPosition++;
-//         slides.style.transform = `translateX(-${currentPosition * step * 3}px)`;
-
-//         if (currentPosition === Math.floor(postItems.length / 3) - 1 && postItems.length < posts.length) {
-
-//             for (let i = currentPosition * 3; i < (currentPosition + 3) * 3 && i < posts.length; i++) {
-
-//             const newPostItem = document.createElement("div");
-//             newPostItem.classList.add("post-item");
-//             newPostItem.innerHTML = `<a href="${posts[i].link}">
-//             <img src="${posts[i]._embedded["wp:featuredmedia"][0].source_url}" alt="${posts[i].title.rendered}">
-//             <h3>${posts[(postItems.length * 3) - 1].title.rendered}</h3>
-//           </a>`;
-//          slides.append(newPostItem);
-//           postItems.push(newPostItem);
-//          }
-//           updateMaxPosition();
-//         }
-
-//       }
-//     });
-//   });
