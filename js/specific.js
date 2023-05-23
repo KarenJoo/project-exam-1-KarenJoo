2d1f941fdc498006ebee4571c519790b027c57df
Karen Jo <karenfiksdahl@gmail.com>
2023-05-23 22:44:30 +0200

    failing attempts with modal

--- js/specific.js
+++ js/specific.js
@@ -1,5 +1,9 @@
 const specificContainer = document.querySelector(".specific-blog-container");
+const modal = document.querySelector(".modal");
+const modalImage = modal.querySelector(".modal-image");
+const modalClose = modal.querySelector(".close");
 const postURL = "https://health-hub.karenjo.no/wp-json/wp/v2/posts?_embed";
+const imgURL = "https://health-hub.karenjo.no/wp-json/wp/v2/media/";
 
 const queryString = document.location.search;
 
@@ -7,7 +11,7 @@ const params = new URLSearchParams(queryString);
 
 const id = params.get("id");
 
-// get single post
+// Fetch and display single post
 async function fetchPost() {
   try {
     const response = await fetch(postURL);
@@ -16,7 +20,9 @@ async function fetchPost() {
     // get single post id
     const specificPost = post.find((post) => post.id === parseInt(id));
 
-    console.log(specificPost);
+    const imgResponse = await fetch(imgURL + specificPost.featured_media);
+    const imgData = await imgResponse.json();
+    const imageURL = imgData.source_url;
 
     // loader
     specificContainer.innerHTML = "";
@@ -28,7 +34,7 @@ async function fetchPost() {
       year: "numeric",
     });
 
-    specificContainer.innerHTML += `<div class="specific-content">
+    specificContainer.innerHTML = `<div class="specific-content">
                                     <h1>${specificPost.title.rendered}</h1><p>Published on ${date}</p>
                                     <div class="content">${specificPost.content.rendered}</div>
                                     </div>`;
@@ -36,6 +42,29 @@ async function fetchPost() {
     const newPageTitle = `Health Hub | ${specificPost.slug}`;
     console.log(newPageTitle);
     document.title = newPageTitle;
+
+    const images = specificContainer.querySelectorAll(".content img");
+
+    // event listener to add each img
+
+    images.forEach((image) => {
+      image.addEventListener("click", () => {
+        console.log("Image clicked!");
+        modalImage.src = image.src;
+        modal.classList.add("show");
+      });
+    });
+
+    // event listener for closing the modal (chatGPT)
+    modalClose.addEventListener("click", () => {
+      modal.classList.remove("show");
+    });
+
+    modal.addEventListener("click", (event) => {
+      if (event.target === modal) {
+        modal.classList.remove("show");
+      }
+    });
   } catch (error) {
     console.log(error);
     specificContainer.innerHTML = "An error occurred";
